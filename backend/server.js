6bde610693eb5ed9e59e98e2f87e936986325812
Sh-Farhan <farhan.mh.shaikh@gmail.com>
2025-05-14 20:22:10 +0530

    server file setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
 
-app.get("/", (req,res) => {
+console.log(PORT);
+
+app.use(express.json());
+app.use(cors()); 
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/test", (req,res) => {
+    console.log(res.getHeaders());
     res.send("Hello from the backend");
 });
 
