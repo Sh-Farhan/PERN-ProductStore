81fe8a14e0c9ed2f332e25795f756a41419e64ed
Sh-Farhan <farhan.mh.shaikh@gmail.com>
2025-05-15 19:40:21 +0530

    routing setup

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,12 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import productRoutes from "./routes/productRoutes.js"
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 console.log(PORT);
 
@@ -16,11 +18,13 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/test", (req,res) => {
-    console.log(res.getHeaders());
-    res.send("Hello from the backend");
-});
+// app.get("/test", (req,res) => {
+//     console.log(res.getHeaders());
+//     res.send("Hello from the backend");
+// });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
 });
