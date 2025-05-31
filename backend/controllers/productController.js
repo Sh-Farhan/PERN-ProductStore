f5699cc53a696d7f1c7dff6032f88231959ee8d8
Sh-Farhan <farhan.mh.shaikh@gmail.com>
2025-05-31 23:36:54 +0530

    setting controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,2 +1,11 @@
-export const getAllProducts = async (req, res) => {};
+import { sql } from "../config/db";
+
+
+export const getProducts = async (req, res) => {
+    
+};
 export const createProduct = async (req, res) => {};
+
+export const getProduct = async (req, res) => {};
+export const updateProduct = async (req, res) => {};
+export const deleteProduct = async (req, res) => {};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { getAllProducts, createProduct } from "../controllers/productController.js";
+import { getProducts, createProduct, deleteProduct, getProduct, updateProduct } from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get("/", getAllProducts);
+router.get("/", getProducts);
+router.get("/:id", getProduct);
 router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.put("/:id", deleteProduct)
 
 export default router;
