1f907f21cf2baab6124a7d724374a97a5440742e
Sh-Farhan <farhan.mh.shaikh@gmail.com>
2025-06-01 21:20:13 +0530

    defining getting and inserting products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,40 @@
-import { sql } from "../config/db";
+import { sql } from "../config/db.js";
 
 
 export const getProducts = async (req, res) => {
-    
+    try {
+        const products = await sql`
+         SELECT * FROM products
+         ORDER BY created_at DESC
+        `;
+
+        console.log("Fetched products", products);
+        res.status(200).json({success: true, data: products})
+    } catch (error) {
+        console.log("Error getProducts ", error);
+        res.status(500).json({success: false, message: "Internal server error"});
+    }
 };
-export const createProduct = async (req, res) => {};
+export const createProduct = async (req, res) => {
+    const {name, price, image} = req.body;
+
+    if(!name || !price || !image){
+        return res.status(400).json({success: false, message: "All fields are required"});
+    }
+
+    try {
+        const newProduct = await sql`
+         INSERT INTO products (name, price, image)
+         VALUES (${name}, ${price}, ${image})
+         RETURNING *
+        `
+        console.log("new product added: ", newProduct);
+        res.status(201).json({success: true, data: newProduct[0]});
+    } catch (error) {
+        console.log("Error in createProduct function ", error);
+        res.status(500).json({success: false, message: "Internal server error"});
+    }
+};
 
 export const getProduct = async (req, res) => {};
 export const updateProduct = async (req, res) => {};
