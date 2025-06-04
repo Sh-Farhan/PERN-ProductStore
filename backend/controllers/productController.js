7e2cf522e3bfb4ac25a760609e3c9b1221bd1dec
Sh-Farhan <farhan.mh.shaikh@gmail.com>
2025-06-04 23:22:21 +0530

    update products api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -49,5 +49,29 @@ export const getProduct = async (req, res) => {
         res.status(500).json({success: false, message: "Internal server error"});
     }
 };
-export const updateProduct = async (req, res) => {};
+export const updateProduct = async (req, res) => {
+    const {id} = req.params;
+    const {name, price, image} = req.body;
+
+    try {
+        const updateProduct = await sql`
+            UPDATE products
+            SET name=${name}, price=${price}, images=${image};
+            WHERE id=${id}
+            RETURNING *
+        `
+
+        if(updateProduct.length === 0){
+            return res.status(404).json({
+                success: false,
+                message: "Product not found",
+            });
+        }
+
+        res.status(200).json({success: true, data: updateProduct[0]});
+    } catch (error) {
+        console.log("Error in updateProduct function ", error);
+        res.status(500).json({success: false, message: "Internal server error"});
+    }
+};
 export const deleteProduct = async (req, res) => {};
